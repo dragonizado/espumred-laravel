4236764ede6fcf0290e6a0fed3f341ad8bf85813
Dragonizado <jesusfrancisco@hotmail.es>
2018-01-02 22:13:30 -0500

    Creacion del modulo pedido con el modelo y la migracion a medias :D

---
+++ app/Modules/Pedidos/Providers/ModuleServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Modules\Pedidos\Providers;
+
+use Caffeinated\Modules\Support\ServiceProvider;
+
+class ModuleServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the module services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadTranslationsFrom(__DIR__.'/../Resources/Lang', 'pedidos');
+        $this->loadViewsFrom(__DIR__.'/../Resources/Views', 'pedidos');
+        $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations', 'pedidos');
+    }
+
+    /**
+     * Register the module services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->register(RouteServiceProvider::class);
+    }
+}
