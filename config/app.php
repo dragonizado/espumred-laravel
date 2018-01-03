893122e77a4927e2bc29788df5a9ceac919d4375
EmmanuelSW <edhgt89@hotmail.com>
2018-01-02 20:43:13 -0500

    Install caffeinated/modules package

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
 		Illuminate\Validation\ValidationServiceProvider::class,
 		Illuminate\View\ViewServiceProvider::class,
 		TCG\Voyager\VoyagerServiceProvider::class,
+		Caffeinated\Modules\ModulesServiceProvider::class,
 		/*
 			* Package Service Providers...
 			*/
@@ -225,7 +226,7 @@ return [
 		'URL' => Illuminate\Support\Facades\URL::class,
 		'Validator' => Illuminate\Support\Facades\Validator::class,
 		'View' => Illuminate\Support\Facades\View::class,
-
+		'Module' => Caffeinated\Modules\Facades\Module::class,
 	],
 
 ];
