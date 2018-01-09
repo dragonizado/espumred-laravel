266027d7da21def7e4b6b01564760a20b63b8f1e
EmmanuelSW <edhgt89@hotmail.com>
2018-01-08 22:24:20 -0500

    Js conditions

--- public/modules/pedidos/main.js
+++ public/modules/pedidos/main.js
@@ -1,5 +1,12 @@
 $('input, select').not('#tipo_pedido').prop('disabled', true);
 
 $("#tipo_pedido").change(function(){
-	$('input, select').not('#tipo_pedido').prop('disabled', false);
+	var tipo_pedido = $(this).val();
+
+	if(tipo_pedido == 2 || tipo_pedido == 3) {
+		$('#listas_precios').prop('disabled', false);
+	} else {
+		$('#listas_precios').prop('disabled', true);
+	}
+	$('input, select').not('#tipo_pedido, #listas_precios').prop('disabled', false);
 });
